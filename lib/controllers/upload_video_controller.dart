9f3333c60c795d5196025f314346cef40ce6fce6
sarthakms7 <mishrasarthak44@gmail.com>
2023-08-28 14:36:07 +0530

    Initial commit

---
+++ lib/controllers/upload_video_controller.dart
@@ -0,0 +1,79 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:get/get.dart';
+import 'package:video_testing_app/constants.dart';
+import 'package:video_testing_app/models/video.dart';
+import 'package:video_compress/video_compress.dart';
+
+class UploadVideoController extends GetxController {
+  _compressVideo(String videoPath) async {
+    final compressedVideo = await VideoCompress.compressVideo(
+      videoPath,
+      quality: VideoQuality.MediumQuality,
+    );
+    return compressedVideo!.file;
+  }
+
+  Future<String> _uploadVideoToStorage(String id, String videoPath) async {
+    Reference ref = firebaseStorage.ref().child('videos').child(id);
+
+    UploadTask uploadTask = ref.putFile(File(videoPath));
+    TaskSnapshot snap = await uploadTask;
+    String downloadUrl = await snap.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
+  _getThumbnail(String videoPath) async {
+    final thumbnail = await VideoCompress.getFileThumbnail(videoPath);
+    return thumbnail;
+  }
+
+  Future<String> _uploadImageToStorage(String id, String videoPath) async {
+    Reference ref = firebaseStorage.ref().child('thumbnails').child(id);
+    UploadTask uploadTask = ref.putFile(await _getThumbnail(videoPath));
+    TaskSnapshot snap = await uploadTask;
+    String downloadUrl = await snap.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
+  // upload video
+  uploadVideo(String songName, String caption, String videoPath) async {
+    try {
+      String uid = '1KbaIwpEC5OsW3u73VgJm7Tw4Zr1';
+      DocumentSnapshot userDoc =
+          await firestore.collection('users').doc(uid).get();
+      // get id
+      var allDocs = await firestore.collection('videos').get();
+      int len = allDocs.docs.length;
+      String videoUrl = await _uploadVideoToStorage("Video $len", videoPath);
+      String thumbnail = await _uploadImageToStorage("Video $len", videoPath);
+
+      Video video = Video(
+        username: 'sarthakms7',
+        uid: uid,
+        id: "Video $len",
+        likes: [],
+        commentCount: 0,
+        shareCount: 0,
+        songName: songName,
+        caption: caption,
+        videoUrl: videoUrl,
+        profilePhoto:
+            'https://firebasestorage.googleapis.com/v0/b/test-b9dee.appspot.com/o/profilePics%2F1KbaIwpEC5OsW3u73VgJm7Tw4Zr1?alt=media&token=9e91759b-ed1e-4e5a-8deb-53334e08a369',
+        thumbnail: thumbnail,
+      );
+
+      await firestore.collection('videos').doc('Video $len').set(
+            video.toJson(),
+          );
+      Get.back();
+    } catch (e) {
+      Get.snackbar(
+        'Error Uploading Video',
+        e.toString(),
+      );
+    }
+  }
+}
